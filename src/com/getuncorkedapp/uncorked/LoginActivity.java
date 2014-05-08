760f6dafde78a22c6f93ebd60134450e8f16d9e3
Loran Briggs <loranbriggs@gmail.com>
2014-05-08 13:48:13 -0700

    fist commit, login screen started

---
+++ src/com/getuncorkedapp/uncorked/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.getuncorkedapp.uncorked;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.getuncorkedapp.R;
+
+public class LoginActivity extends Activity {
+	
+	private EditText	usernameField;
+	private EditText	passwordField;
+	private Button		registerButton;
+	private Button		loginButton;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        usernameField = (EditText) findViewById(R.id.username);
+        passwordField = (EditText) findViewById(R.id.password);
+        registerButton = (Button) findViewById(R.id.register);
+        loginButton = (Button) findViewById(R.id.login);
+        
+        loginButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				usernameField.setText("You clicked login");
+			}
+		});
+    }
+
+}
