49ac4f9a03ea0d5475e8b01f66c4a4686e6939a5
Alexander Smithson <alexander-nye@hotmail.com>
2014-05-23 22:51:21 -0700

    Added a bit to get views, but unsure...
    on most economical query option.

--- src/com/getuncorkedapp/activities/ViewWineActivity.java
+++ src/com/getuncorkedapp/activities/ViewWineActivity.java
@@ -3,6 +3,9 @@
  */
 package com.getuncorkedapp.activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,21 +14,27 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.getuncorkedapp.R;
+import com.getuncorkedapp.models.Review;
 import com.getuncorkedapp.models.Wine;
 import com.getuncorkedapp.utils.WebImageView;
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseQuery.CachePolicy;
 
 /**
  * @author Alex
+ * @author Loran
  *
  */
 public class ViewWineActivity extends Activity {
 	
 	private String wineId;
 	private Wine wine;
+	private ArrayList<Review> reviewList;
 	
 	private TextView wineName;
 	private WebImageView wineIcon;
@@ -64,7 +73,7 @@ public class ViewWineActivity extends Activity {
 
 	}
 
-	/* (non-Javadoc)
+	/* (non-Javadoc) 
 	 * @see android.app.Activity#onPause()
 	 */
 	@Override
@@ -101,6 +110,32 @@ public class ViewWineActivity extends Activity {
 			}
         });
     }
+	
+	public void getReviews(Wine wine)
+	{
+		ParseRelation<Review> relation = wine.getRelation("reviews");
+		ParseQuery<Review> queryUser = relation.getQuery();
+		queryUser.addAscendingOrder("createdAt");
+		ParseQuery<Review> queryNotUser = relation.getQuery();
+		
+		
+		
+//		query.findInBackground(new FindCallback<Review>() {
+//
+//			@Override
+//			public void done(List<Review> list, ParseException e) {
+//				if(e == null)
+//				{
+//					list.add(0, list.re);
+//				}
+//				else
+//				{
+//					Log.w("Review Query", e.getMessage());
+//				}
+//			}
+//			
+//		});
+	}
 	
 	public void fillInWine(Wine wine) {
 		wineName.setText(wine.getName());
