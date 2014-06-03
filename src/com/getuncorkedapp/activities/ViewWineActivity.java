e98725a0d9afaa254a356a83b84c74070e04c7e8
Yoyo <yehchengyuan@gmail.com>
2014-06-03 12:09:04 -0700

    added checkbox, fix the focus on login

--- src/com/getuncorkedapp/activities/ViewWineActivity.java
+++ src/com/getuncorkedapp/activities/ViewWineActivity.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
